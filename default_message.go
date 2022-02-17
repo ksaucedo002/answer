295cbf308a8031c113b8490daca0c658a6cccec8
Kevin Saucedo <user002_@outlook.com>
2022-02-17 14:19:55 -0500

    new default messages

--- default_message.go
+++ default_message.go
@@ -1,6 +1,15 @@
 package answer
 
+import "github.com/ksaucedo002/answer/errores"
+
 const (
-	SUCCESS_OPERATION = "operación realizada con éxito"
-	ERROR_OPERATION   = "no se pudo completar la operacion"
+	SUCCESS_OPERATION    = "operación realizada con éxito"
+	SUCCESS_CREATE       = "Registro guardado correctamente"
+	ERROR_OPERATION      = "no se pudo completar la operacion"
+	FORBIDDEN_OPERATION  = "operación no permitida"
+	ERROR_INTERNAL_ERROR = "error, algo paso"
+)
+
+var (
+	ErrorDefaultForbiden = errores.NewForbiddenf(nil, FORBIDDEN_OPERATION)
 )
